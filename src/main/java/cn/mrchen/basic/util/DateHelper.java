09a7a02fb6b6ee255941c8b5946fe5b1a2a6797a
喔喔和奶糖 <1002940887@qq.com>
2019-07-22 11:05:15 +0800

    日期计算方法 DateHelper.dateCalculation

--- src/main/java/cn/mrchen/basic/util/DateHelper.java
+++ src/main/java/cn/mrchen/basic/util/DateHelper.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class DateHelper {
@@ -13,7 +14,9 @@ public class DateHelper {
      * @param date
      * @param days
      * @return
+     * @deprecated 建议使用dateCalculation(Date date, ChronoUnit chronoUnit, long size)
      */
+    @Deprecated
     public static Date calculateByDays(Date date, int days) {
         boolean isAdd = true;
         if (date == null) {
@@ -32,6 +35,37 @@ public class DateHelper {
             localDateTime = localDateTime.plusDays(days);
         } else {
             localDateTime = localDateTime.minusDays(days);
+        }
+        ZonedDateTime zonedDateTime = localDateTime.atZone(zoneId);
+        Date newDate = Date.from(zonedDateTime.toInstant());
+        return newDate;
+    }
+
+    /**
+     *
+     * @param date
+     * @param chronoUnit
+     * @param size
+     * @return
+     */
+    public static Date dateCalculation(Date date, ChronoUnit chronoUnit, long size) {
+        boolean isAdd = true;
+        if (date == null || chronoUnit == null) {
+            throw new RuntimeException("Date or unit of account is empty, cannot be calculated");
+        } else if (size == 0) {
+            return (Date) date.clone();
+        } else if (size < 0) {
+            isAdd = false;
+            size = size * (-1);
+        }
+
+        Instant instant = date.toInstant();
+        ZoneId zoneId = ZoneId.systemDefault();
+        LocalDateTime localDateTime = instant.atZone(zoneId).toLocalDateTime();
+        if (isAdd) {
+            localDateTime = localDateTime.plus(size, chronoUnit);
+        } else {
+            localDateTime = localDateTime.minus(size, chronoUnit);
         }
         ZonedDateTime zonedDateTime = localDateTime.atZone(zoneId);
         Date newDate = Date.from(zonedDateTime.toInstant());
---
+++ src/test/java/cn/mrchen/basic/util/DateHelperTest.java
@@ -0,0 +1,21 @@
+package cn.mrchen.basic.util;
+
+import org.junit.Test;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class DateHelperTest {
+
+    @Test
+    public void dateCalculationTest01() {
+        Date now = new Date(System.currentTimeMillis());
+        Date after = DateHelper.dateCalculation(now, ChronoUnit.YEARS, -7);
+        after = DateHelper.dateCalculation(after, ChronoUnit.MONTHS, -5);
+        after = DateHelper.dateCalculation(after, ChronoUnit.DAYS, 7);
+        now = after;
+        after = DateHelper.dateCalculation(after, ChronoUnit.MONTHS, 1);
+        System.out.println(now.toString());
+        System.out.println(after.toString());
+    }
+}
